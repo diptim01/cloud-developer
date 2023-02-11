d8e0bbd67b080ef1dcc67144b1c887f1f9a69383
Oladipo.Alokan <oladipotimothy69@gmail.com>
2023-02-11 15:57:33 +0100

    database configuration

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,18 +1,26 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    // "username": "udagramdev2",
+    // "password": "udagramdev2",
+    // "database": "udagramdev2",
+    // "host": "udagramdev2.ce6roocnqgqt.us-east-1.rds.amazonaws.com",
+    // "dialect": "postgres",
+    // "aws_region": "us-east-1",
+    // "aws_profile": "default",
+    // "aws_media_bucket": "udagram-dipo-dev"
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET
   },
   "jwt": {
     "secret": " "
   },
-  "prod": {
+  "prod": { 
     "username": "",
     "password": "",
     "database": "udagram_prod",
